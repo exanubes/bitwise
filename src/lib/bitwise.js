657bb0f4eafffe1dbeb1ef395c9882db06760459
Exanubes <blog@exanubes.com>
2026-02-16 08:56:50 +0000

    Implement bitwise and

--- src/lib/bitwise.js
+++ src/lib/bitwise.js
@@ -23,7 +23,19 @@ const OPERATIONS = {
  * @param {Integer} b
  * @returns {Integer}
  */
-export function and(_a, _b) {}
+export function and(_a, _b) {
+    const [a, b] = normalize_binary(_a.binary(), _b.binary());
+    let result = '';
+    for (let index = 0; index < a.length; index += 1) {
+        if (a[index] === '1' && b[index] === '1') {
+            result += '1';
+        } else {
+            result += '0';
+        }
+    }
+
+    return Integer.from_binary(result);
+}
 
 /**
  * @param {Integer} a
@@ -52,3 +64,21 @@ function lshift(a, b) {}
  * @returns {Integer}
  */
 function rshift(a, b) {}
+
+/**
+ * @param {string} a
+ * @param {string} b
+ */
+export function normalize_binary(a, b) {
+    if (a.length === b.length) {
+        return [a, b];
+    }
+
+    if (a.length > b.length) {
+        b = b.padStart(a.length, '0');
+    } else {
+        a = a.padStart(b.length, '0');
+    }
+
+    return [a, b];
+}
